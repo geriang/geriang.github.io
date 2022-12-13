c0495ec22ab473978f67b352873063100f5cf75b
gerillima@gmail.com <gerillima@gmail.com>
2022-12-13 23:26:46 +0800

    implemented and tested new function that retrieves address using postal code. Used innerHTML to show address

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ async function main () {
 }
 main ();
 
-document.querySelector("#button").addEventListener("click", async function(){
+document.querySelector('input[type="postalCode"]').addEventListener("keyup", async function(){
     getPostalCode = document.querySelector('input[type="postalCode"]').value;
     let add = await loadOneMapDataAddress(getPostalCode)
     console.log(add)
-    let oldAddress = document.querySelector('input[type="address"]').value;
-    oldAddress = add;
-
+    if (getPostalCode.length == 6){
+    let oldAddress = document.getElementById("address")
+    oldAddress.innerHTML = `<div id="address" class="form-floating">
+    <input type="address" class="form-control" id="floatingInput" name="address" placeholder="Address" value="${add}">
+    <label for="floatingInput">Address</label>
+  </div>`
+    }
+    else {
+        let oldAddress = document.getElementById("address")
+        oldAddress.innerHTML = `<div id="address" class="form-floating">
+        <input type="address" class="form-control" id="floatingInput" name="address" placeholder="Address">
+        <label for="floatingInput">Address</label>
+        </div>`
+            }
 })
+
